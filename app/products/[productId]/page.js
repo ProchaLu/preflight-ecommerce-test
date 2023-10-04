82886c0a23339da5412956cf1b43cc9be3feeca1
Judit Langer <judit.langer@aon.at>
2023-10-04 12:10:01 +0200

    Expand single product page

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,27 +1,52 @@
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
-import { getProduct } from '../../../database/products';
+import { getProductById } from '../../../database/products';
 import AddQuantityToCart from './AddQuantityToCart';
+import styles from './page.module.scss';
 
-export function generateMetadata({ params }) {
-  const singleProduct = getProduct(Number(params.productId));
+export async function generateMetadata({ params }) {
+  const singleProduct = await getProductById(Number(params.productId));
 
   return {
     title: singleProduct ? singleProduct.name : '',
   };
 }
 
-export default function Product({ params }) {
-  const singleProduct = getProduct(Number(params.productId));
+export default async function Product({ params }) {
+  const singleProduct = await getProductById(Number(params.productId));
 
   if (!singleProduct) {
     return notFound();
   }
   return (
-    <div>
-      <h1>{singleProduct.name}</h1>
-      {/* <Image src=`images/${singleProduct.firstName data-test-id="product-image"}` */}
-      {/* <p data-test-id="product-price">NUMBER WITHOUT CURRENCY</p> */}
-      <AddQuantityToCart productId={singleProduct.id} />
-    </div>
+    <section className={styles.productContent}>
+      <Image
+        data-test-id="product-image"
+        src={`/images/productImages/${singleProduct.name}.png`}
+        alt="coffee beans product-image"
+        width={600}
+        height={600}
+      />
+      <div className={styles.productDescription}>
+        <h1 className={styles.h1}>{singleProduct.name}</h1>
+        <p>
+          {' '}
+          <span className={styles.label}>Price:</span>
+          <span data-test-id="product-price">{singleProduct.price}</span>€
+        </p>
+        <p>
+          <span className={styles.label}>Origin:</span> {singleProduct.origin}
+        </p>
+        <p>
+          <span className={styles.label}>Type:</span> {singleProduct.type}
+        </p>
+        <p>
+          <span className={styles.label}>Flavour profile:</span>{' '}
+          {singleProduct.flavourProfile}
+        </p>
+        <p>{singleProduct.description}</p>
+        <AddQuantityToCart productId={singleProduct.id} />
+      </div>
+    </section>
   );
 }
