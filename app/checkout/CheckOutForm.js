9c5cbbffcb621126fd4665bbee7b25f7a3955d5a
Judit Langer <judit.langer@aon.at>
2023-10-04 12:07:51 +0200

    Add patterns to input fields

--- app/checkout/CheckOutForm.js
+++ app/checkout/CheckOutForm.js
@@ -1,17 +1,30 @@
 'use client';
 
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { clearCookies } from './actions';
 
 export default function CheckOutForm() {
   return (
     <form>
       <div>
         <label htmlFor="firstName">First name:</label>
-        <input data-test-id="checkout-first-name" id="firstName" required />
+        <input
+          data-test-id="checkout-first-name"
+          id="firstName"
+          pattern="\p{Letter}+"
+          required
+          placeholder="First name"
+        />
       </div>
       <div>
         <label htmlFor="lastName">Last name:</label>
-        <input data-test-id="checkout-last-name" id="lastName" required />
+        <input
+          data-test-id="checkout-last-name"
+          id="lastName"
+          pattern="\p{Letter}+"
+          required
+          placeholder="Last name"
+        />
       </div>
       <div>
         <label htmlFor="email">E-mail-address:</label>
@@ -19,19 +32,43 @@ export default function CheckOutForm() {
       </div>
       <div>
         <label htmlFor="address">Address:</label>
-        <input data-test-id="checkout-address" id="address" required />
+        <input
+          data-test-id="checkout-address"
+          id="address"
+          pattern="[\p{Letter}0-9./-]+"
+          required
+          placeholder="Address"
+        />
       </div>
       <div>
         <label htmlFor="city">City:</label>
-        <input data-test-id="checkout-city" id="city" required />
+        <input
+          data-test-id="checkout-city"
+          id="city"
+          pattern="\p{Letter}+"
+          required
+          placeholder="City"
+        />
       </div>
       <div>
         <label htmlFor="postalCode">Postal code:</label>
-        <input data-test-id="checkout-postal-code" id="postalCode" required />
+        <input
+          data-test-id="checkout-postal-code"
+          id="postalCode"
+          pattern="[0-9]+"
+          required
+          placeholder="Postal code"
+        />
       </div>
       <div>
         <label htmlFor="country">Country:</label>
-        <input data-test-id="checkout-country" id="country" required />
+        <input
+          data-test-id="checkout-country"
+          id="country"
+          pattern="\p{Letter}+"
+          required
+          placeholder="Country"
+        />
       </div>
       <div>
         <label htmlFor="creditCard">Credit card:</label>
@@ -40,7 +77,9 @@ export default function CheckOutForm() {
           id="creditCard"
           maxLength="16"
           minLength="16"
+          pattern="[0-9]+"
           required
+          placeholder="Card number"
         />
       </div>
       <div>
@@ -48,6 +87,8 @@ export default function CheckOutForm() {
         <input
           data-test-id="checkout-expiration-date"
           id="expirationDate"
+          pattern="^(0[1-9]|1[0-2])\/?([0-9]{2})$"
+          placeholder="MM / YY"
           required
         />
       </div>
@@ -58,18 +99,22 @@ export default function CheckOutForm() {
           id="securityCode"
           maxLength="3"
           minLength="3"
+          pattern="[0-9]+"
           required
+          placeholder="CVC"
         />
       </div>
       <div>
-        <button formAction data-test-id="checkout-confirm-order">
+        <button
+          formAction={async () => {
+            await clearCookies();
+            redirect('/thankyou');
+          }}
+          data-test-id="checkout-confirm-order"
+        >
           Confirm Order
         </button>
       </div>
     </form>
   );
 }
-
-// The Confirm Order button needs to have the HTML attribute data-test-id="checkout-confirm-order"
-
-// Clicking on the Confirm Order button should empty the cart and navigate to the Thank You page
