6dc956968394a8efdad4688ad11a4a1cdb7dfa5d
Judit Langer <judit.langer@aon.at>
2023-10-04 12:08:59 +0200

    Add button styling

--- app/products/[productId]/AddQuantityToCart.js
+++ app/products/[productId]/AddQuantityToCart.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { addQuantity } from './actions';
+import styles from './AddQuantityToCart.module.scss';
 
 export default function SetSelectedQuantity(props) {
   const [selectedQuantity, setSelectedQuantity] = useState(1);
@@ -14,8 +15,10 @@ export default function SetSelectedQuantity(props) {
         min="1"
         data-test-id="product-quantity"
         onChange={(event) => setSelectedQuantity(event.currentTarget.value)}
+        className={styles.input}
       />
       <button
+        className={styles.cartButton}
         formAction={async () =>
           await addQuantity(props.productId, Number(selectedQuantity))
         }
