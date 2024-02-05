9496bef3fe2f1fdd5ba5127b32b4c93a4662e0b8
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-05 16:44:18 +0100

    Create folders for PostgreSQL and set permissions for postgres user

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -7,6 +7,7 @@ echo "Setting up PostgreSQL on Alpine Linux..."
 
 echo "Creating folders for PostgreSQL and adding permissions for postgres user..."
 export PGDATA=/postgres-volume/run/postgresql/data
+mkdir -p "$PGDATA"
 
 # Only allow postgres user access to data directory
 chmod 700 "$PGDATA"
