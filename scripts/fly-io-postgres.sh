354df7f065d0db611874bc16fad8e5986ce2c332
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-05 16:40:26 +0100

    Add executable permission and change ownership of PostgreSQL setup script

--- scripts/fly-io-postgres.sh
+++ scripts/fly-io-postgres.sh
@@ -4,4 +4,6 @@ echo "Setting up PostgreSQL on Fly.io..."
 mkdir -p /postgres-volume/run/postgresql/data
 chown postgres:postgres /postgres-volume/run/postgresql
 chown postgres:postgres /app/scripts/alpine-postgresql-setup-and-start.sh
+chmod +x /app/scripts/alpine-postgresql-setup-and-start.sh
+su -c "chown postgres:postgres /postgres-volume/run/postgresql/data"
 su postgres -c "/app/scripts/alpine-postgresql-setup-and-start.sh"
