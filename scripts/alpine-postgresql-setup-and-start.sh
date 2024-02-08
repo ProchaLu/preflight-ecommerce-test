d88c3c46da44b549e1346f9a35e655fafa0d0987
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-08 11:52:32 +0100

    Fix PostgreSQL data directory permissions and update config path

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -9,12 +9,13 @@ echo "Adding permissions for postgres user..."
 export PGDATA=/postgres-volume/run/postgresql/data
 
 # Only allow postgres user access to data directory
-chown -R postgres:postgres "$PGDATA"
+chmod 0700 "$PGDATA"
 initdb -D "$PGDATA"
 
 # Update PostgreSQL config path to use volume location if app has a volume
 sed -i "s|#unix_socket_directories = '/run/postgresql'|unix_socket_directories = '/postgres-volume/run/postgresql/'|g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase changes not saved in migrations)"
 
+# TODO fix, after permission denied error is resolved
 # Log to syslog, which is rotated (older logs automatically deleted)
 # sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i "$PGDATA/postgresql.conf"
 
