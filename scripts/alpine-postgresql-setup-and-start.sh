04b86d8bffff302312cb19e50d766e22e12dc115
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-08 10:21:35 +0100

    Update PostgreSQL config path and add log file

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -12,13 +12,12 @@ export PGDATA=/postgres-volume/run/postgresql/data
 chmod 0700 "$PGDATA"
 initdb -D "$PGDATA"
 
-# print file content from /postgres-volume/run/postgresql/data/postgresql.conf
-cat /postgres-volume/run/postgresql/data/postgresql.conf
-
 # Update PostgreSQL config path to use volume location if app has a volume
 sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql/'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase changes not saved in migrations)"
 
 cat /postgres-volume/run/postgresql/data/postgresql.conf
+
+cat /postgres-volume/run/postgresql/data/.s.PGSQL.5432.lock
 
 # Log to syslog, which is rotated (older logs automatically deleted)
 # sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i "$PGDATA/postgresql.conf"
