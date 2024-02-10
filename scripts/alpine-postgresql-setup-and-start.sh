549c53abbd43f3455d0f5710ce05d2de1073fd27
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-10 11:07:33 +0100

    Update configuration files and scripts

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -4,20 +4,15 @@
 set -o errexit
 
 echo "Setting up PostgreSQL on Alpine Linux..."
-
-echo "Adding permissions for postgres user..."
 export PGDATA=/postgres-volume/run/postgresql/data
 
 # Only allow postgres user access to data directory
+echo "Adding permissions for postgres user..."
 chmod 0700 "$PGDATA"
 initdb -D "$PGDATA"
 
 # Update PostgreSQL config path to use volume location if app has a volume
-sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase changes not saved in migrations)"
-
-# TODO fix, after permission denied error is resolved
-# Log to syslog, which is rotated (older logs automatically deleted)
-sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i "$PGDATA/postgresql.conf"
+sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf
 
 # Configure PostgreSQL to listen for connections from any address
 echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
@@ -26,7 +21,7 @@ echo "Starting PostgreSQL..."
 pg_ctl start -D "$PGDATA"
 
 echo "Creating database, user and schema..."
-psql -h /postgres-volume/run/postgresql  -U postgres postgres << SQL
+psql -h  $PGDATA -U postgres postgres << SQL
   CREATE DATABASE $PGDATABASE;
   CREATE USER $PGUSERNAME WITH ENCRYPTED PASSWORD '$PGPASSWORD';
   GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSERNAME;
